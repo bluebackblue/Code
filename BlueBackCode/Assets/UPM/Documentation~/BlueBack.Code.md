e51ac2be5be7b91af06c18964a92eaf1bf917516
blueback <blueback28@gmail.com>
2022-05-30 21:46:00 +0900

    整理

--- BlueBackCode/Assets/UPM/Documentation~/BlueBack.Code.md
+++ BlueBackCode/Assets/UPM/Documentation~/BlueBack.Code.md
@@ -18,7 +18,7 @@ Unity 2022.1.0b16
 
 ## UPM
 ### 最新
-* https://github.com/bluebackblue/UpmCode.git?path=BlueBackCode/Assets/UPM#0.0.20
+* https://github.com/bluebackblue/UpmCode.git?path=BlueBackCode/Assets/UPM#0.0.21
 ### 開発
 * https://github.com/bluebackblue/UpmCode.git?path=BlueBackCode/Assets/UPM
 
--- BlueBackCode/Assets/UPM/Runtime/BlueBack/Code/Convert.cs
+++ BlueBackCode/Assets/UPM/Runtime/BlueBack/Code/Convert.cs
@@ -23,20 +23,21 @@ namespace BlueBack.Code
 
 			string --> replacelist --> string
 
-			a_replace_list	: 置換リスト。
-			a_template		: 元データ。
+			a_replace_list			: 置換リスト。
+			a_data					: データ。
+			a_loop					: ループ回数。
 
 		*/
-		public static string Replace(System.Collections.Generic.Dictionary<string,string> a_replace_list,string a_template)
+		public static string Replace(System.Collections.Generic.Dictionary<string,string> a_replace_list,string a_data,int a_loop)
 		{
-			string t_string = a_template;
+			string t_data = a_data;
 
-			for(int ii=0;ii<16;ii++){
+			for(int ii=0;ii<a_loop;ii++){
 				bool t_change = false;
 				foreach(System.Collections.Generic.KeyValuePair<string,string> t_pair in a_replace_list){
-					string t_string_new = t_string.Replace(t_pair.Key,t_pair.Value);
-					if(t_string != t_string_new){
-						t_string = t_string_new;
+					string t_string_new = t_data.Replace(t_pair.Key,t_pair.Value);
+					if(t_data != t_string_new){
+						t_data = t_string_new;
 						t_change = true;
 					}
 				}
@@ -45,31 +46,58 @@ namespace BlueBack.Code
 				}
 			}
 
-			return t_string;
+			return t_data;
+		}
+
+		/** 置き換え。
+
+			a_replace_list			: 置換リスト。
+			a_data					: データ。
+
+		*/
+		public static void Replace(System.Collections.Generic.Dictionary<string,string> a_replace_list,string[] a_data)
+		{
+			int ii_max = a_data.Length;
+			for(int ii=0;ii<ii_max;ii++){
+				a_data[ii] = Replace(a_replace_list,a_data[ii],16);
+			}
+		}
+
+		/** 置き換え。
+
+			a_replace_list			: 置換リスト。
+			a_data					: データ。
+
+		*/
+		public static void Replace(System.Collections.Generic.Dictionary<string,string> a_replace_list,System.Collections.Generic.List<string> a_data)
+		{
+			int ii_max = a_data.Count;
+			for(int ii=0;ii<ii_max;ii++){
+				a_data[ii] = Replace(a_replace_list,a_data[ii],16);
+			}
 		}
 
 		/** 追加。
 
 			string[] --> Add --> List<string>
 
-			a_out_list		: 追加先。
-			a_replace_list	: 置換リスト。
-			a_template		: 元データ。
+			a_out_list				: 追加先。
+			a_replace_list			: 置換リスト。
+			a_data					: データ。
 
 		*/
-		public static void Add(System.Collections.Generic.List<string> a_out_list,System.Collections.Generic.Dictionary<string,string> a_replace_list,string[] a_template)
+		public static void Add(System.Collections.Generic.List<string> a_out_list,System.Collections.Generic.Dictionary<string,string> a_replace_list,string[] a_data)
 		{
-			int ii_max = a_template.Length;
-
+			int ii_max = a_data.Length;
 			if(a_replace_list != null){
 				for(int ii=0;ii<ii_max;ii++){
-					string t_temp = a_template[ii];
-					t_temp = Replace(a_replace_list,t_temp);
+					string t_temp = a_data[ii];
+					t_temp = Replace(a_replace_list,t_temp,16);
 					a_out_list.Add(t_temp);
 				}
 			}else{
 				for(int ii=0;ii<ii_max;ii++){
-					string t_temp = a_template[ii];
+					string t_temp = a_data[ii];
 					a_out_list.Add(t_temp);
 				}
 			}
@@ -79,23 +107,23 @@ namespace BlueBack.Code
 
 			List<string> --> Add --> List<string>
 
-			a_out_list		: 追加先。
-			a_replace_list	: 置換リスト。
-			a_template		: 元データ。
+			a_out_list				: 追加先。
+			a_replace_list			: 置換リスト。
+			a_data					: データ。
 
 		*/
-		public static void Add(System.Collections.Generic.List<string> a_out_list,System.Collections.Generic.Dictionary<string,string> a_replace_list,System.Collections.Generic.List<string> a_template)
+		public static void Add(System.Collections.Generic.List<string> a_out_list,System.Collections.Generic.Dictionary<string,string> a_replace_list,System.Collections.Generic.List<string> a_data)
 		{
-			int ii_max = a_template.Count;
+			int ii_max = a_data.Count;
 			if(a_replace_list != null){
 				for(int ii=0;ii<ii_max;ii++){
-					string t_temp = a_template[ii];
-					t_temp = Replace(a_replace_list,t_temp);
+					string t_temp = a_data[ii];
+					t_temp = Replace(a_replace_list,t_temp,16);
 					a_out_list.Add(t_temp);
 				}
 			}else{
 				for(int ii=0;ii<ii_max;ii++){
-					string t_temp = a_template[ii];
+					string t_temp = a_data[ii];
 					a_out_list.Add(t_temp);
 				}
 			}
@@ -103,71 +131,71 @@ namespace BlueBack.Code
 
 		/** 複製。
 
-			「a_template」を「a_count」の数だけ複製、「a_index_key」をインデックスに置換。
-
-			a_out_list		: 追加先。
-			a_index_key		: 例 : <<INDEX>>
-			a_index_count	: 総数。
-			a_index_offset	: 開始インデックス。
-			a_replace_list	: 置換リスト。
-			a_template		: 元データ。
-
-		*/
-		public static void Duplicate(System.Collections.Generic.List<string> a_out_list,string a_index_key,int a_index_count,int a_index_offset,System.Collections.Generic.Dictionary<string,string> a_replace_list,string[] a_template)
-		{
-			int ii_max = a_index_count + a_index_offset;
-			if(a_replace_list != null){
-				for(int ii=a_index_offset;ii<ii_max;ii++){
-					string ii_string = ii.ToString();
-					int jj_max = a_template.Length;
-					for(int jj=0;jj<jj_max;jj++){
-						string t_temp = a_template[jj].Replace(a_index_key,ii_string);
-						t_temp = Replace(a_replace_list,t_temp);
-						a_out_list.Add(t_temp);
-					}
-				}
-			}else{
-				for(int ii=a_index_offset;ii<ii_max;ii++){
-					string ii_string = ii.ToString();
-					int jj_max = a_template.Length;
-					for(int jj=0;jj<jj_max;jj++){
-						string t_temp = a_template[jj].Replace(a_index_key,ii_string);
-						a_out_list.Add(t_temp);
-					}
-				}
-			}
-		}
-
-		/** 複製。
-
-			「a_template」を「a_count」の数だけ複製、「a_index_key」をインデックスに置換。
+			「a_data」を「a_count」の数だけ複製、「a_index_key」をインデックスに置換。
 
 			a_out_list				: 追加先。
 			a_index_key				: 例 : <<INDEX>>
 			a_index_count			: 総数。
 			a_index_offset			: 開始インデックス。
 			a_replace_list			: 置換リスト。
-			a_template_callback		: 元データ。
+			a_data					: データ。
 
 		*/
-		public static void Duplicate(System.Collections.Generic.List<string> a_out_list,string a_index_key,int a_index_count,int a_index_offset,System.Collections.Generic.Dictionary<string,string> a_replace_list,System.Func<IndexType,string[]> a_template_callback)
+		public static void Duplicate(System.Collections.Generic.List<string> a_out_list,string a_index_key,int a_index_count,int a_index_offset,System.Collections.Generic.Dictionary<string,string> a_replace_list,string[] a_data)
 		{
 			int ii_max = a_index_count + a_index_offset;
 			if(a_replace_list != null){
 				for(int ii=a_index_offset;ii<ii_max;ii++){
 					string ii_string = ii.ToString();
-					string[] t_template = a_template_callback(ii);
-					int jj_max = t_template.Length;
+					int jj_max = a_data.Length;
 					for(int jj=0;jj<jj_max;jj++){
-						string t_temp = t_template[jj].Replace(a_index_key,ii_string);
-						t_temp = Replace(a_replace_list,t_temp);
+						string t_temp = a_data[jj].Replace(a_index_key,ii_string);
+						t_temp = Replace(a_replace_list,t_temp,16);
 						a_out_list.Add(t_temp);
 					}
 				}
 			}else{
 				for(int ii=a_index_offset;ii<ii_max;ii++){
 					string ii_string = ii.ToString();
-					string[] t_template = a_template_callback(ii);
+					int jj_max = a_data.Length;
+					for(int jj=0;jj<jj_max;jj++){
+						string t_temp = a_data[jj].Replace(a_index_key,ii_string);
+						a_out_list.Add(t_temp);
+					}
+				}
+			}
+		}
+
+		/** 複製。
+
+			「a_data」を「a_count」の数だけ複製、「a_index_key」をインデックスに置換。
+
+			a_out_list				: 追加先。
+			a_index_key				: 例 : <<INDEX>>
+			a_index_count			: 総数。
+			a_index_offset			: 開始インデックス。
+			a_replace_list			: 置換リスト。
+			a_data_creator			: データ作成。
+
+		*/
+		public static void Duplicate(System.Collections.Generic.List<string> a_out_list,string a_index_key,int a_index_count,int a_index_offset,System.Collections.Generic.Dictionary<string,string> a_replace_list,System.Func<IndexType,string[]> a_data_creator)
+		{
+			int ii_max = a_index_count + a_index_offset;
+			if(a_replace_list != null){
+				for(int ii=a_index_offset;ii<ii_max;ii++){
+					string ii_string = ii.ToString();
+					string[] t_template = a_data_creator(ii);
+					int jj_max = t_template.Length;
+					for(int jj=0;jj<jj_max;jj++){
+						string t_temp = t_template[jj].Replace(a_index_key,ii_string);
+						t_temp = Replace(a_replace_list,t_temp,16);
+						a_out_list.Add(t_temp);
+					}
+				}
+			}else{
+				for(int ii=a_index_offset;ii<ii_max;ii++){
+					string ii_string = ii.ToString();
+					string[] t_template = a_data_creator(ii);
 					int jj_max = t_template.Length;
 					for(int jj=0;jj<jj_max;jj++){
 						string t_temp = t_template[jj].Replace(a_index_key,ii_string);
@@ -179,35 +207,35 @@ namespace BlueBack.Code
 
 		/** 複製。
 
-			「a_template」を「a_count」の数だけ複製、「a_index_key」をインデックスに置換。
+			「a_data」を「a_count」の数だけ複製、「a_index_key」をインデックスに置換。
 
-			a_out_list		: 追加先。
-			a_index_key		: 例 : <<INDEX>>
-			a_index_count	: 総数。
-			a_index_offset	: 開始インデックス。
-			a_replace_list	: 置換リスト。
-			a_template		: 元データ。
+			a_out_list				: 追加先。
+			a_index_key				: 例 : <<INDEX>>
+			a_index_count			: 総数。
+			a_index_offset			: 開始インデックス。
+			a_replace_list			: 置換リスト。
+			a_data					: データ。
 
 		*/
-		public static void Duplicate(System.Collections.Generic.List<string> a_out_list,string a_index_key,int a_index_count,int a_index_offset,System.Collections.Generic.Dictionary<string,string> a_replace_list,System.Collections.Generic.List<string> a_template)
+		public static void Duplicate(System.Collections.Generic.List<string> a_out_list,string a_index_key,int a_index_count,int a_index_offset,System.Collections.Generic.Dictionary<string,string> a_replace_list,System.Collections.Generic.List<string> a_data)
 		{
 			int ii_max = a_index_count + a_index_offset;
 			if(a_replace_list != null){
 				for(int ii=a_index_offset;ii<ii_max;ii++){
 					string ii_string = ii.ToString();
-					int jj_max = a_template.Count;
+					int jj_max = a_data.Count;
 					for(int jj=0;jj<jj_max;jj++){
-						string t_temp = a_template[jj].Replace(a_index_key,ii_string);
-						t_temp = Replace(a_replace_list,t_temp);
+						string t_temp = a_data[jj].Replace(a_index_key,ii_string);
+						t_temp = Replace(a_replace_list,t_temp,16);
 						a_out_list.Add(t_temp);
 					}
 				}
 			}else{
 				for(int ii=a_index_offset;ii<ii_max;ii++){
 					string ii_string = ii.ToString();
-					int jj_max = a_template.Count;
+					int jj_max = a_data.Count;
 					for(int jj=0;jj<jj_max;jj++){
-						string t_temp = a_template[jj].Replace(a_index_key,ii_string);
+						string t_temp = a_data[jj].Replace(a_index_key,ii_string);
 						a_out_list.Add(t_temp);
 					}
 				}
@@ -220,22 +248,22 @@ namespace BlueBack.Code
 
 			a_out_stringbuilder		: 追加先。
 			a_replace_list			: 置換リスト。
-			a_template				: 元データ。
+			a_data					: データ。
 
 		*/
-		public static void Add(System.Text.StringBuilder a_out_stringbuilder,System.Collections.Generic.Dictionary<string,string> a_replace_list,string[] a_template)
+		public static void Add(System.Text.StringBuilder a_out_stringbuilder,System.Collections.Generic.Dictionary<string,string> a_replace_list,string[] a_data)
 		{
-			int ii_max = a_template.Length;
+			int ii_max = a_data.Length;
 			if(a_replace_list != null){
 				for(int ii=0;ii<ii_max;ii++){
-					string t_temp = a_template[ii];
-					t_temp = Convert.Replace(a_replace_list,t_temp);
+					string t_temp = a_data[ii];
+					t_temp = Convert.Replace(a_replace_list,t_temp,16);
 					a_out_stringbuilder.Append(t_temp);
 					a_out_stringbuilder.Append("\n");
 				}
 			}else{
 				for(int ii=0;ii<ii_max;ii++){
-					string t_temp = a_template[ii];
+					string t_temp = a_data[ii];
 					a_out_stringbuilder.Append(t_temp);
 					a_out_stringbuilder.Append("\n");
 				}
@@ -248,22 +276,22 @@ namespace BlueBack.Code
 
 			a_out_stringbuilder		: 追加先。
 			a_replace_list			: 置換リスト。
-			a_template				: 元データ。
+			a_data					: データ。
 
 		*/
-		public static void Add(System.Text.StringBuilder a_out_stringbuilder,System.Collections.Generic.Dictionary<string,string> a_replace_list,System.Collections.Generic.List<string> a_template)
+		public static void Add(System.Text.StringBuilder a_out_stringbuilder,System.Collections.Generic.Dictionary<string,string> a_replace_list,System.Collections.Generic.List<string> a_data)
 		{
-			int ii_max = a_template.Count;
+			int ii_max = a_data.Count;
 			if(a_replace_list != null){
 				for(int ii=0;ii<ii_max;ii++){
-					string t_temp = a_template[ii];
-					t_temp = Convert.Replace(a_replace_list,t_temp);
+					string t_temp = a_data[ii];
+					t_temp = Convert.Replace(a_replace_list,t_temp,16);
 					a_out_stringbuilder.Append(t_temp);
 					a_out_stringbuilder.Append("\n");
 				}
 			}else{
 				for(int ii=0;ii<ii_max;ii++){
-					string t_temp = a_template[ii];
+					string t_temp = a_data[ii];
 					a_out_stringbuilder.Append(t_temp);
 					a_out_stringbuilder.Append("\n");
 				}
